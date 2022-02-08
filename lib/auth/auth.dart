d40710eb23a87556d4a35d334d427eae4f3b2b14
rupaak <rupakt525@gmail.com>
2022-02-08 23:53:53 +0545

    firebase intergation and our user model

---
+++ lib/auth/auth.dart
@@ -0,0 +1,22 @@
+import 'package:coffee/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  //create user object based on firebaseuser
+  OurUser? _userFromFirebaseUser(User user){
+    return user != null ? OurUser(uid: user.uid) : null;
+  }
+
+  Future signupAnnom() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
